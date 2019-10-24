5fdf1539836e39d781e92217ba6fd6a724c7532e
camposLuis <luis.campos.henrique@hotmail.com>
2019-10-23 22:42:15 -0300

    Configurando redux e redux-saga

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import { YellowBox, StatusBar } from 'react-native';
 // import 'react-native-gesture-handler';
 import './config/ReactotronConfig';
 
+import { store, persistor } from './store';
 import Routes from './routes';
 
 YellowBox.ignoreWarnings(['CTRootView cancelTouches']);
 
 export default function src() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#131415" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#131415" />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }
