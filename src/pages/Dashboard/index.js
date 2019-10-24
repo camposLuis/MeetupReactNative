abac4c7d63fc635aa7fb68a2196a5a619348b93c
camposLuis <luis.campos.henrique@hotmail.com>
2019-10-24 01:18:57 -0300

    Estilização da tabBar

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 // import { Container } from './styles';
 
 export default function Dashboard() {
   return <View />;
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Meetups',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="list" size={20} color={tintColor} />
+  ),
+};
